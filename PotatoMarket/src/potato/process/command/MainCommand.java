40e7db1051aabbb265870ec9b5fff3a879143bbd
savesoo <hsw2657@gmail.com>
2022-10-24 14:53:00 +0900

    메인 커맨드 추가
    로그인 후 전체글 보기

--- PotatoMarket/src/potato/process/command/MainCommand.java
+++ PotatoMarket/src/potato/process/command/MainCommand.java
@@ -1,10 +1,15 @@
 package potato.process.command;
 
 import potato.controller.EndpointController;
+import potato.controller.board.BoardPrintController;
 
 public class MainCommand extends AbsCommand {
 
 	public MainCommand(){
-		command.put(5, new EndpointController());
+		command.put(1, new BoardPrintController()); // 전체글 보기
+		//command.put(2, )// 판매글
+		//command.put(3, ); // 구매내역
+		//command.put(4, ); // 회원 정보
+		command.put(5, new EndpointController()); // 종료
 	}
 }

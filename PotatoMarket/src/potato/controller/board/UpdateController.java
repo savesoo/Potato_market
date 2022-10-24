46453f4ba4af010484bc0e1ba6865f47292b6542
himediabyul <himedia.byul@gmail.com>
2022-10-24 15:57:01 +0900

    Create UpdateController.java

---
+++ PotatoMarket/src/potato/controller/board/UpdateController.java
@@ -0,0 +1,47 @@
+package potato.controller.board;
+
+import potato.domain.Board;
+import potato.service.UpdateService;
+import potato.controller.IController;
+
+
+public class UpdateController implements IController {
+	
+	UpdateService service = new UpdateService();
+	
+	public void process() {
+		
+		String userid = null;
+		int writedate=0;
+		boolean salestatus = true;
+		
+		System.out.println("게시글 수정이 시작됩니다.");
+		
+		System.out.println("수정할 게시글번호를 입력해주세요. >> ");
+		int boardid = Integer.parseInt(Main.nextLine());
+
+		
+		System.out.println("카테고리>> /n(1.생활용품, 2.패션/잡화, 3.전자제품, 4.도서, 5.반려동물용품, 6.기타) ");
+		int category = Integer.parseInt(Main.nextLine());
+		
+		System.out.println("판매물품 >> ");
+		String product = Main.nextLine();
+		
+		System.out.println("판매금액 >> ");
+		int saleprice = Integer.parseInt(Main.nextLine());
+		
+		System.out.println("거래지역 >> /n (ex.서울시=>서울, 영양군=>영양)");
+		String tradeloc = Main.nextLine();
+		
+		int result = service.update(new Board(boardid, userid, category, product, writedate, saleprice, salestatus, tradeloc));
+		
+	
+		if(result > 0) {
+			System.out.println("수정되었습니다.");
+		} else {
+			System.out.println("수정 실패!");
+		}
+		
+	}
+
+}

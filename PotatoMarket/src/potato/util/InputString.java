510f68df48c9deb6e4e6c53d31928e476ff34938
Seo-Aram <arams.office@gmail.com>
2022-10-24 10:56:48 +0900

    Update InputString.java

--- PotatoMarket/src/potato/util/InputString.java
+++ PotatoMarket/src/potato/util/InputString.java
@@ -13,10 +13,14 @@ public class InputString {
 	
 	public static int inputInt() {
 		int result = -1;
-		try {
-			result = Integer.parseInt(scan.nextLine());
-		} catch(Exception e) {
-			System.out.println("입력 형식이 잘못 되었습니다. 숫자 형식으로 다시 입력해주세요.");
+		while(true) {
+			try {
+				result = Integer.parseInt(scan.nextLine());
+				break;
+			} catch(Exception e) {
+				System.out.println("입력 형식이 잘못 되었습니다. 숫자 형식으로 다시 입력해주세요.");
+				continue;
+			}
 		}
 		return result;
 	}

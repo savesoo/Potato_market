a6725896155a743299ea933b986c8c109901fd3f
Seo-Aram <arams.office@gmail.com>
2022-10-24 17:23:06 +0900

    Update MainCommand.java

--- PotatoMarket/src/potato/process/command/MainCommand.java
+++ PotatoMarket/src/potato/process/command/MainCommand.java
@@ -1,11 +1,14 @@
 package potato.process.command;
 
+import java.util.HashMap;
+
 import potato.controller.EndpointController;
 import potato.controller.board.BoardPrintController;
 
 public class MainCommand extends AbsCommand {
 
 	public MainCommand(){
+		command = new HashMap<>();
 		command.put(1, new BoardPrintController()); // 전체글 보기
 		//command.put(2, )// 판매글
 		//command.put(3, ); // 구매내역

7e579de037dae420693ca990ac27feaa10a333a1
Seo-Aram <arams.office@gmail.com>
2022-10-24 15:59:56 +0900

    코드 주석처리

--- PotatoMarket/src/potato/controller/board/DeleteController.java
+++ PotatoMarket/src/potato/controller/board/DeleteController.java
@@ -6,22 +6,22 @@ import potato.service.DeleteService;
 public class DeleteController implements IController {
 	
 	DeleteService service = new DeleteService();
-	
-	public void process() {
 
-
-		System.out.println("게시글 삭제가 진행됩니다.");
+	@Override
+	public boolean process() {
+		/*
+		 * System.out.println("게시글 삭제가 진행됩니다.");
+		 * 
+		 * System.out.println("삭제할 게시글 번호 >>"); int boardid =
+		 * Integer.parseInt(Main.sc.nextLine());
+		 * 
+		 * int result = service.delete(boardid);
+		 * 
+		 * if(result > 0) { System.out.println("삭제되었습니다."); } else {
+		 * System.out.println("삭제 실패"); }
+		 */
 		
-		System.out.println("삭제할 게시글 번호 >>");
-		int boardid = Integer.parseInt(Main.sc.nextLine());
-		
-		int result = service.delete(boardid);
-		
-		if(result > 0) {
-			System.out.println("삭제되었습니다.");			
-		} else {
-			System.out.println("삭제 실패");
-		}
+		return false;
 		
 	}
 
--- PotatoMarket/src/potato/controller/board/InsertController.java
+++ PotatoMarket/src/potato/controller/board/InsertController.java
@@ -9,34 +9,35 @@ public class InsertController implements IController  {
 
 	InsertService service = new InsertService();
 	
-	public void process() {
+	@Override
+	public boolean process() {
 		
-		int boardid=0;
-		String userid=null;
-		int writedate = 0;
-		boolean salestatus= true;
+		/*
+		 * int boardid=0; String userid=null; int writedate = 0; boolean salestatus=
+		 * true;
+		 * 
+		 * System.out.println("판매글 입력을 시작합니다.");
+		 * 
+		 * System.out.
+		 * println("카테고리>> /n(1.생활용품, 2.패션/잡화, 3.전자제품, 4.도서, 5.반려동물용품, 6.기타) "); int
+		 * category = Integer.parseInt(Main.nextLine());
+		 * 
+		 * System.out.println("판매물품 >> "); String product = Main.nextLine();
+		 * 
+		 * System.out.println("판매금액 >> "); int saleprice =
+		 * Integer.parseInt(Main.nextLine());
+		 * 
+		 * System.out.println("거래지역 >> /n (ex.서울시=>서울, 영양군=>영양)"); String tradeloc =
+		 * Main.nextLine();
+		 * 
+		 * int result = service.insert(new Board(boardid, userid, category, product,
+		 * writedate, saleprice, salestatus, tradeloc));
+		 * 
+		 * if(result>0) { System.out.println("입력되었습니다."); } else {
+		 * System.out.println("입력실패"); }
+		 */
 		
-		System.out.println("판매글 입력을 시작합니다.");
-		
-		System.out.println("카테고리>> /n(1.생활용품, 2.패션/잡화, 3.전자제품, 4.도서, 5.반려동물용품, 6.기타) ");
-		int category = Integer.parseInt(Main.nextLine());
-		
-		System.out.println("판매물품 >> ");
-		String product = Main.nextLine();
-		
-		System.out.println("판매금액 >> ");
-		int saleprice = Integer.parseInt(Main.nextLine());
-		
-		System.out.println("거래지역 >> /n (ex.서울시=>서울, 영양군=>영양)");
-		String tradeloc = Main.nextLine();
-		
-		int result = service.insert(new Board(boardid, userid, category, product, writedate, saleprice, salestatus, tradeloc));
-		
-		if(result>0) {
-			System.out.println("입력되었습니다.");
-		} else {
-			System.out.println("입력실패");
-		}
+		return false;
 		
 	}
 	

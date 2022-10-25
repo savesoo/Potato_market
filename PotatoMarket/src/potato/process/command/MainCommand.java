9725c23f0b719dcea3985f890b2c4e6958015271
himediabyul <himedia.byul@gmail.com>
2022-10-25 11:10:22 +0900

    판매 글쓰기 추가

--- PotatoMarket/src/potato/process/command/MainCommand.java
+++ PotatoMarket/src/potato/process/command/MainCommand.java
@@ -3,6 +3,7 @@ package potato.process.command;
 import java.util.HashMap;
 
 import potato.controller.EndpointController;
+import potato.controller.board.InsertController;
 import potato.controller.board.PrintController;
 import potato.controller.sign.UserMenuController;
 
@@ -13,8 +14,9 @@ public class MainCommand extends AbsCommand {
 		command = new HashMap<>();
 		command.put(1, new PrintController()); // 전체글 보기
 		//command.put(2, )// 판매글
-		//command.put(3, ); // 구매내역
-		command.put(4, new UserMenuController()); // 회원 정보
-		command.put(5, new EndpointController()); // 종료
+		command.put(3, new InsertController()); //글쓰기
+		//command.put(4, ); // 구매내역
+		command.put(5, new UserMenuController()); // 회원 정보
+		command.put(6, new EndpointController()); // 종료
 	}
 }

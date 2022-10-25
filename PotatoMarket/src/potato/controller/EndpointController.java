39f667c3be94b27a0b2780a0dc76bd5a93e87e10
Seo-Aram <arams.office@gmail.com>
2022-10-25 14:34:49 +0900

    로그아웃 처리

--- PotatoMarket/src/potato/controller/EndpointController.java
+++ PotatoMarket/src/potato/controller/EndpointController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 import potato.domain.Session;
+import potato.util.InputString;
 
 /**
  * 프로그램 종료 할 때, 파일화 작업(자동 로그인 때문에)이 필요해서 만든 클래스
@@ -13,6 +14,15 @@ public class EndpointController implements IController {
 
 	@Override
 	public boolean process() {
+		System.out.println("로그아웃 하겠습니까?");
+		System.out.print("0. 예 / 그외. 아니오>>");
+		
+		int tryInput = InputString.inputInt();
+		
+		if(tryInput == 0) {
+			Session.getInstance().destroySession();
+		}
+		
 		ObjectOutputStream outStream = null;
 		
 		try { 
@@ -31,7 +41,8 @@ public class EndpointController implements IController {
 					e.printStackTrace();
 				}
 			}
-			
+			System.out.println("프로그램이 정상 종료되었습니다.");
+			System.out.println("이용해주셔서 감사합니다.");
 			System.exit(0);
 		}
 		return true;

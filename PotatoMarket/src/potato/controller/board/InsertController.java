e952275dd27e602c458a81fc390b8bdd850244c7
himediabyul <himedia.byul@gmail.com>
2022-10-25 14:37:49 +0900

    / -> 역슬래시 수정

--- PotatoMarket/src/potato/controller/board/InsertController.java
+++ PotatoMarket/src/potato/controller/board/InsertController.java
@@ -20,7 +20,7 @@ public class InsertController implements IController {
 
 		System.out.println("판매글 입력을 시작합니다.");
 
-		System.out.println("카테고리>> /n(1.생활용품, 2.패션/잡화, 3.전자제품, 4.도서, 5.반려동물용품, 6.기타) ");
+		System.out.println("카테고리>> \n(1.생활용품, 2.패션/잡화, 3.전자제품, 4.도서, 5.반려동물용품, 6.기타) ");
 		int category = InputString.inputInt();
 
 		System.out.println("판매물품 >> ");
@@ -29,7 +29,7 @@ public class InsertController implements IController {
 		System.out.println("판매금액 >> ");
 		int saleprice = InputString.inputInt();
 
-		System.out.println("거래지역 >> /n (ex.서울시=>서울, 영양군=>영양)");
+		System.out.println("거래지역 >> \n (ex.서울시=>서울, 영양군=>영양)");
 		String tradeloc = InputString.inputDefaultString();
 		
 		int result = service.insert(new Board(boardid, userid, category, product, saleprice, salestatus, null, tradeloc));

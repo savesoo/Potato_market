36140bc3c8e45db53c04fae1d8f69a810e3ab87e
himediahj <himedia.hj@gmail.com>
2022-10-25 12:33:31 +0900

    회원 메뉴 이전 화면으로 돌아가는 거 추가

--- PotatoMarket/src/potato/controller/sign/UserMenuController.java
+++ PotatoMarket/src/potato/controller/sign/UserMenuController.java
@@ -13,17 +13,25 @@ public class UserMenuController implements IController {
 		AbsCommand command = new UserCommand();
 		int inputMenu;
 
-		System.out.println();
-		System.out.println("======================");
-		System.out.println("1. 내 정보 보기");
-		System.out.println("2. 내 정보 수정하기");
-		// System.out.println("3. 회원 탈퇴하기");
-		System.out.println("======================");
-		System.out.print("원하시는 작업 번호를 입력해주세요 >> ");
-		inputMenu = InputString.inputInt();
-		command.command.get(inputMenu).process();
+		while (true) {
+			System.out.println();
+			System.out.println("======================");
+			System.out.println("1. 내 정보 보기");
+			System.out.println("2. 내 정보 수정하기");
+			System.out.println("3. 이전 화면으로 돌아가기");
+			System.out.println("======================");
+			System.out.println();
+			System.out.print("원하시는 작업 번호를 입력해주세요 >> ");
+			System.out.println();
+			inputMenu = InputString.inputInt();
 
-		return false;
+			if (inputMenu == 3) {
+
+				return false;
+			}
+
+			command.command.get(inputMenu).process();
+		}
+
 	}
-
 }
